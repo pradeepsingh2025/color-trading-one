77466bbef7aa6199ae63e81d029bf48b12454860
Sardar <sardarsakki@gmail.com>
2025-05-27 01:31:18 +0530

    2nd changes

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header/Header";
+import Poster from "./components/sliding-poster/Poster";
+
+function Home() {
+
+  return (
+   <>
+    
+   </>
+  )
+}
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import Home from "./Home";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />
+);
